f7f59abcb7f7e3010bd3f1a4508821543c5ba219
Sync_Pundit <Sync_Pundit@syncpundit.io>
2025-08-10 03:28:11 +0200

    remove TTY from script

--- run.sh
+++ run.sh
@@ -1,92 +1,84 @@
-#!/bin/bash
-set -eo pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 __dirname=$(cd "$(dirname "$0")"; pwd -P)
 cd "${__dirname}"
 
-platform="Linux" # Assumed
-uname=$(uname)
-case $uname in
-    "Darwin")
-    platform="MacOS / OSX"
-    ;;
-    MINGW*)
-    platform="Windows"
-    ;;
+platform="Linux"
+case "$(uname)" in
+  "Darwin") platform="MacOS / OSX" ;;
+  MINGW*)   platform="Windows" ;;
 esac
 
 usage(){
-  echo "Usage: $0 [--port N]"
+  echo "Usage: $0 [--port N] [--debug] [--api-keys]"
   echo
   echo "Run LibreTranslate using docker."
-  echo
-  exit
+  exit 0
 }
 
-export LT_PORT=5050
+export LT_PORT=${LT_PORT:-5050}
+DB_VOLUME=${DB_VOLUME:-}   # will be set by --api-keys if used
+ARGS=()
 
 # Parse args for overrides
-ARGS=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
-case $key in
+while [[ $# -gt 0 ]]; do
+  case "$1" in
     --port)
-    export LT_PORT="$2"
-    ARGS+=("$1")
-    ARGS+=("$2") # save it in an array for later
-    shift # past argument
-    shift # past value
-    ;;
+      [[ $# -ge 2 ]] || { echo "Missing value for --port"; exit 1; }
+      export LT_PORT="$2"
+      ARGS+=("$1" "$2")
+      shift 2
+      ;;
     --debug)
-    export LT_DEBUG=YES
-    ARGS+=("$1")
-    shift # past argument
-    ;;
+      export LT_DEBUG=YES
+      ARGS+=("$1")
+      shift
+      ;;
     --api-keys)
-    export DB_VOLUME="-v lt-db:/app/db"
-    ARGS+=("$1")
-    shift # past argument
-    ;;
-    --help)
-    usage
-    ;;
-    *)    # unknown option
-    ARGS+=("$1")
-    shift # past argument
-    ;;
-esac
+      export DB_VOLUME="-v lt-db:/app/db"
+      ARGS+=("$1")
+      shift
+      ;;
+    --help|-h) usage ;;
+    *) ARGS+=("$1"); shift ;;
+  esac
 done
 
 # $1 = command | $2 = help_text | $3 = install_command (optional)
 check_command(){
-    hash "$1" 2>/dev/null || not_found=true
-    if [[ $not_found ]]; then
-        check_msg_prefix="Checking for $1... "
-
-        # Can we attempt to install it?
-        if [[ -n "$3" ]]; then
-            echo -e "$check_msg_prefix \033[93mnot found, we'll attempt to install\033[39m"
-            $3 || sudo $3
-
-            # Recurse, but don't pass the install command
-            check_command "$1" "$2"
-        else
-            check_msg_result="\033[91m can't find $1! Check that the program is installed and that you have added the proper path to the program to your PATH environment variable before launching the program. If you change your PATH environment variable, remember to close and reopen your terminal. $2\033[39m"
-        fi
-    else
-        check_msg_prefix="Checking for $1... "
-        check_msg_result="\033[92m found\033[39m"
+  local not_found=
+  if ! hash "$1" 2>/dev/null; then
+    not_found=1
+    if [[ -n "${3:-}" ]]; then
+      echo -e "Checking for $1... \033[93mnot found, attempting install\033[39m"
+      $3 || sudo $3
+      check_command "$1" "$2"          # re-check (no installer this time)
+      return
     fi
+  fi
 
-    echo -e "$check_msg_prefix $check_msg_result"
-    if [[ $not_found ]]; then
-        return 1
-    fi
+  if [[ -n "${not_found:-}" ]]; then
+    echo -e "Checking for $1... \033[91mcan't find $1!\033[39m $2"
+    return 1
+  else
+    echo -e "Checking for $1... \033[92mfound\033[39m"
+  fi
 }
 
 environment_check(){
-    check_command "docker" "https://www.docker.com/"
+  check_command "docker" "https://www.docker.com/"
 }
 
 environment_check
-docker run -ti --rm -p $LT_PORT:$LT_PORT $DB_VOLUME -v lt-local:/home/libretranslate/.local libretranslate/libretranslate ${ARGS[@]}
+
+# Detect TTY and set docker flags accordingly
+DOCKER_FLAGS=(--rm -p "${LT_PORT}:${LT_PORT}")
+[[ -n "${DB_VOLUME}" ]] && DOCKER_FLAGS+=(${DB_VOLUME})
+
+if [[ -t 0 && -t 1 ]]; then
+  # interactive terminal: allow TTY
+  DOCKER_FLAGS=(-i -t "${DOCKER_FLAGS[@]}")
+fi
+
+# Run container (ARGS are passed through to libretranslate)
+exec docker run "${DOCKER_FLAGS[@]}" libretranslate/libretranslate "${ARGS[@]}"
